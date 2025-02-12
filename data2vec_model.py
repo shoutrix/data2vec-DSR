35e0e4be8d1d7e561235fd14b0f1191dfd82de38
shoutrix <dasshoutrik@gmail.com>
2025-02-12 11:01:32 +0530

    bug fix

--- data2vec_model.py
+++ data2vec_model.py
@@ -4,6 +4,8 @@ from transformers import Wav2Vec2Model
 import numpy as np
 from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
+import torch.nn.functional as F
+import copy
 
 
 class D2R(nn.Module):
@@ -26,87 +28,90 @@ class D2R(nn.Module):
     def forward(self, student_input, teacher_input, alpha):
         
         with torch.no_grad():
-            # teacher feature extraction
-            teacher_features = self.teacher.feature_extractor(teacher_input)
-            teacher_features = teacher_features.transpose(1, 2)
-            teacher_hidden_states, teacher_features = self.teacher.feature_projection(teacher_features)
-            
             # student feature extraction
             student_features = self.student.feature_extractor(student_input)
             student_features = student_features.transpose(1, 2)
             student_hidden_states, student_features = self.student.feature_projection(student_features)
             
-            # DTW and teacher feature repeatation
-            print(teacher_hidden_states.shape, student_hidden_states.shape)
-            teacher_hidden_states, student_hidden_states = self.align_with_dtw(teacher_hidden_states, student_hidden_states)
-            print(teacher_hidden_states.shape)
-            
-            # student feature masking
-            # student_hidden_states, masked_indices = self.mask_student_hidden_states(student_hidden_states)
+            # teacher feature extraction
+            teacher_features = self.teacher.feature_extractor(teacher_input)
+            teacher_features = teacher_features.transpose(1, 2)
+            teacher_hidden_states, teacher_features = self.teacher.feature_projection(teacher_features)
 
             # teacher encoder forward
-            # teacher_encoder_out = self.teacher.encoder(teacher_hidden_states, output_hidden_states=True)
-            # teacher_last_hidden_state = teacher_encoder_out.last_hidden_state
-            # teacher_hidden_states = teacher_encoder_out.hidden_states
+            teacher_encoder_out = self.teacher.encoder(teacher_hidden_states, output_hidden_states=True)
+            teacher_last_hidden_state = teacher_encoder_out.last_hidden_state
+            teacher_hidden_states = teacher_encoder_out.hidden_states
             
+        # student feature masking
+        student_hidden_states, mask = self.mask_student_hidden_states(student_hidden_states)
+        # mask : shape(B, T) True where masked
             
         # student encoder forward
-        # student_encoder_out = self.student.encoder(student_hidden_states, output_hidden_states=True)
-        # student_last_hidden_state = student_encoder_out.last_hidden_state
-        # student_hidden_states = student_encoder_out.hidden_states
+        student_encoder_out = self.student.encoder(student_hidden_states, output_hidden_states=True)
+        student_last_hidden_state = student_encoder_out.last_hidden_state
+        student_hidden_states = student_encoder_out.hidden_states
+        
+        
+        print(len(student_hidden_states))
+        print(student_hidden_states[-1])
+        print(student_last_hidden_state)
         
         # MSE loss over several layers of teacher and student
-        # mse_loss = self.compute_mse_loss(teacher_hidden_states, student_hidden_states)
-        
-        # (optional) compute Cross-Contrastive-Loss
-        # cc_loss = self.compute_cc_loss(teacher_last_hidden_state, student_last_hidden_state, masked_indices)
-        
-        # compute total loss
-        # loss = alpha * mse_loss + (1-alpha) * cc_loss
-        # return loss
+        layer_loss, mean_loss = self.compute_mse_loss(teacher_hidden_states, student_hidden_states, mask)
 
+        # compute total loss
+        return mean_loss
+    
+    
     @classmethod
     def align_with_dtw(cls, teacher_features, student_features):
-
         B, T1, d = teacher_features.shape
         _, T2, _ = student_features.shape
 
         if T1 == T2:
-            return teacher_features, student_features
-        if T1 < T2:
-            small_seq, large_seq = teacher_features, student_features
-        else:
-            small_seq, large_seq = student_features, teacher_features
+            return teacher_features
 
-        _, T_small, _ = small_seq.shape
-        _, T_large, _ = large_seq.shape
-
-        expanded_small_seq = torch.zeros((B, T_large, d), dtype=small_seq.dtype, device=small_seq.device)
+        expanded_teacher_features = torch.zeros_like(student_features)
 
         for b in range(B):
-            _, path = fastdtw(small_seq[b].cpu().numpy(), large_seq[b].cpu().numpy(), dist=euclidean)
-            print(path)
-            small_indices, large_indices = zip(*path)
+            _, path = fastdtw(teacher_features[b].detach().cpu().numpy(), student_features[b].detach().cpu().numpy(), dist=euclidean)
+            
+            # print(path)
 
-            expanded_seq = torch.zeros((T_large, d), dtype=small_seq.dtype, device=small_seq.device)
-            for xi, yi in zip(small_indices, large_indices):
-                expanded_seq[yi] += small_seq[b, xi]
-
-            expanded_small_seq[b] = expanded_seq
-
-        if T1 < T2:
-            return expanded_small_seq, student_features
-        else:
-            return teacher_features, expanded_small_seq
+            teacher_indices, student_indices = zip(*path)
+            teacher_indices = torch.tensor(teacher_indices, dtype=torch.long, device=teacher_features.device)
+            student_indices = torch.tensor(student_indices, dtype=torch.long, device=teacher_features.device)
+            expanded_teacher_features[b] = expanded_teacher_features[b] + teacher_features[b, teacher_indices]
+        return expanded_teacher_features
 
         
 
-    def mask_student_hidden_states(self):
-        return None
+    def mask_student_hidden_states(self, student_hidden_states):
+        B, T, _ = student_hidden_states.shape
+        mask = torch.ones((B, T), dtype=torch.bool)
+        return student_hidden_states, mask
     
     
-    def compute_mse_loss(self, teacher_hidden_states, student_hidden_states):
-        return None
+    def compute_mse_loss(self, teacher_hidden_states, student_hidden_states, mask):
+        
+        dim_ = teacher_hidden_states[0].shape[-1]
+        layer_loss = {}
+        
+        for layer_idx in self.MSE_loss_layers:
+            teacher_hidden_state_layer = teacher_hidden_states[layer_idx]
+            student_hidden_state_layer = student_hidden_states[layer_idx]
+            teacher_hidden_state_layer = self.align_with_dtw(teacher_hidden_state_layer, student_hidden_state_layer)
+            teacher_hidden_state_layer = teacher_hidden_state_layer[mask].view(-1, dim_)
+            student_hidden_state_layer = student_hidden_state_layer[mask].view(-1, dim_)
+            
+            loss = F.mse_loss(teacher_hidden_state_layer, student_hidden_state_layer, reduce="mean")
+            layer_loss[layer_idx] = loss
+            
+            print(loss)
+            
+        mean_loss = torch.tensor(list(layer_loss.values())).mean()
+        return layer_loss, mean_loss
     
     
     def compute_cc_loss(self, teacher_last_hidden_state, student_last_hidden_state, masked_indices):
